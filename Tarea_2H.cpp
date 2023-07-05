08c510be6f39b601852f3719b8a12325ea55fd97
TheAbrahamX <137915275+TheAbrahamX@users.noreply.github.com>
2023-07-04 19:10:58 -0600

    Homeworks

---
+++ Tarea_2H.cpp
@@ -0,0 +1,12 @@
+//Programa delvolver Ultimo elemento
+#include<iostream>
+using namespace std;
+int devolverUltimoElemento(int array[]){
+	// Devuelve el último elemento de un array
+    // Tu código:
+	cout <<"Ultimo elemento: "<< array[4] << endl;
+}
+int main(){
+	int variables[5]={1,2,3,4,5};
+	devolverUltimoElemento(variables);
+}
