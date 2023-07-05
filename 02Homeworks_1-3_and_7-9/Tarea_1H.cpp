98da57d5dafe1dbd7af136857a1fdfeedebefac5
TheAbrahamX <137915275+TheAbrahamX@users.noreply.github.com>
2023-07-04 19:32:16 -0600

    Update Tarea_1H.cpp

--- 02Homeworks_1-3_and_7-9/Tarea_1H.cpp
+++ 02Homeworks_1-3_and_7-9/Tarea_1H.cpp
@@ -1,1 +1,14 @@
+//Programa devolver Primer elemento
+#include<iostream>
+using namespace std;
 
+int devolverPrimerElemento(int array[]){
+	// Devuelve el primer elemento de un  array (pasado por parametro)
+    // Tu código:
+	cout <<"Primer elemento: "<< array[0] << endl;
+}
+
+int main(){
+	int variables[5]={1,2,3,4,5};
+	devolverPrimerElemento(variables);
+}
